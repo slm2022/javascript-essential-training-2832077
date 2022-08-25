3eb3d239f271638a2ebb803df02ab7d313b78140
SLM2022 <smasud@usf.edu>
2022-08-24 19:49:49 -0500

    .

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,11 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const laptop = {
+  color: "black",
+  screen: { size: 15, resolution: "4k" },
+  keyboard: { numOfKeys: 35, color: "black", language: "ENG" },
+  weight: 5,
+  brand: "Lenovo",
+};
