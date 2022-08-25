2688cb9a3e63b18eeff935384c0b630bf5d96519
SLM2022 <smasud@usf.edu>
2022-08-24 20:13:55 -0500

    practice 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -13,5 +13,54 @@ const laptop = {
   screen: { size: 15, resolution: "4k" },
   keyboard: { numOfKeys: 35, color: "black", language: "ENG" },
   weight: 5,
+  boughtNew: true,
   brand: "Lenovo",
+  changeScreenInfo: function (newSize, newResolution) {
+    this.screen.size = newSize;
+    this.screen.resolution = newResolution;
+  },
+  changeKeyboardInfo: function (newNumOfKeys, newColor, newLanguage) {
+    this.keyboard.numOfKeys = newNumOfKeys;
+    this.keyboard.color = newColor;
+    this.keyboard.language = newLanguage;
+  },
+  changeBoughtNew: function (newBoughtNewFlag) {
+    this.boughtNew = newBoughtNewFlag;
+  },
 };
+
+console.log("Laptop Color:", laptop.color);
+console.log(
+  "Screen Res:",
+  laptop.screen.resolution,
+  "   -- Screen Size:",
+  laptop.screen.size
+);
+console.log(
+  "Keyboard Language:",
+  laptop.keyboard.language,
+  "   -- Keyboard Color:",
+  laptop.keyboard.color,
+  "   -- Keyboard Num of Keys:",
+  laptop.keyboard.numOfKeys
+);
+
+laptop.changeKeyboardInfo(40, "red", "FRA");
+laptop.changeBoughtNew(false);
+laptop.changeScreenInfo(13, "1080p");
+
+console.log("Laptop Color:", laptop.color);
+console.log(
+  "Screen Res:",
+  laptop.screen.resolution,
+  "   -- Screen Size:",
+  laptop.screen.size
+);
+console.log(
+  "Keyboard Language:",
+  laptop.keyboard.language,
+  "   -- Keyboard Color:",
+  laptop.keyboard.color,
+  "   -- Keyboard Num of Keys:",
+  laptop.keyboard.numOfKeys
+);
